ff73cdec4741af2a4034678a10c5d8b0e98fdb52
shrutiP1 <shrutipatil13798@gmail.com>
2021-09-01 06:36:50 +0530

    finsih our loco framewrok

---
+++ podProject/podProject/Service.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  podProject
+//
+//  Created by gadetzone on 01/09/21.
+//
+
+import Foundation
+
+public class Service {
+    private init() {}
+    
+    public static func doSomething()->String {
+         return "Youa re here ..."
+    }
+}
---
+++ podProject/podProject/podProject.h
@@ -0,0 +1,18 @@
+//
+//  podProject.h
+//  podProject
+//
+//  Created by gadgetzone on 01/09/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for podProject.
+FOUNDATION_EXPORT double podProjectVersionNumber;
+
+//! Project version string for podProject.
+FOUNDATION_EXPORT const unsigned char podProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <podProject/PublicHeader.h>
+
+
